3a53ddb87147889a7107baec3c8a022350c802fc
Junsik <junsik213@naver.com>
2024-10-02 15:49:38 +0900

    공유 참조와 사이드 이펙트

---
+++ src/lang/immutable/address/RefMain1_2.java
@@ -0,0 +1,17 @@
+package lang.immutable.address;
+
+public class RefMain1_2 {
+
+    public static void main(String[] args) {
+        // RefMain1_1의 문제를 해결하기 위해서 객체를 각각 생성
+        Address a = new Address("서울"); // x001
+        Address b = new Address("서울"); // x002
+        System.out.println("a: " + a);
+        System.out.println("b: " + b);
+
+        b.setValue("부산");
+        System.out.println("부산 -> b");
+        System.out.println("a: " + a);
+        System.out.println("b: " + b);
+    }
+}
